1c6633ada1e100a2f3484422906dd4ee8043cd35
thesilverbear <johannmoramira@gmail.com>
2023-02-20 16:05:24 -0300

    Cambio al CSS

--- app/db.py
+++ app/db.py
@@ -7,12 +7,12 @@ from .schema import instructions
 
 def get_db():                       
     if 'db' not in g:               
-        g.db = mysql.connector.connect(      
-            port=current_app.config['DATABASE_PORT'],          
+        g.db = mysql.connector.connect(                
             host=current_app.config['DATABASE_HOST'], 
             user=current_app.config['DATABASE_USER'],
             password=current_app.config['DATABASE_PASSWORD'],
-            database=current_app.config['DATABASE']
+            database=current_app.config['DATABASE'],
+            port=current_app.config['DATABASE_PORT']
         ) 
         g.c = g.db.cursor(dictionary=True)
     return g.db, g.c 
